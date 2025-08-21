f18adc997ae28b85061c74826bac473169bb3b77
Ahsan Alam <ahsansfiverr@gmail.com>
2025-08-21 12:52:48 +0600

    shift added

--- app/Models/WorkingShift.php
+++ app/Models/WorkingShift.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class WorkingShift extends Model
 {
     protected $table = "working_shifts";
-    protected $fillable = ["id", "employee_id", "shift_name", "start_time", "end_time", "grace_time", "created_at", "updated_at"];
+    protected $fillable = ["id","shift_name", "start_time", "end_time", "grace_time", "created_at", "updated_at"];
 
     public function employees()
     {
--- database/migrations/2025_08_21_064133_create_working_shifts_table.php
+++ database/migrations/2025_08_21_064133_create_working_shifts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('working_shifts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('employee_id')->constrained()->cascadeOnDelete();
             $table->string('shift_name');
             $table->time('start_time');
             $table->time('end_time');
