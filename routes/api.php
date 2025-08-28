b4544ae99cd1f260ae77a4d934578714e6cfd942
Ahsan Alam <ahsansfiverr@gmail.com>
2025-08-28 10:48:51 +0600

    added extra api routes

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,16 @@
 
 use App\Http\Controllers\Api\AttendanceController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ShiftController;
 use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\DocumentController;
+use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\EmployeeController;
+use App\Http\Controllers\Api\HolidayController;
 use App\Http\Controllers\Api\LeaveController;
+use App\Http\Controllers\Api\NoticeController;
 use App\Http\Controllers\Api\PayrollController;
+use App\Models\employee as Employee;
 use Illuminate\Support\Facades\Route;
 
 
@@ -21,6 +26,9 @@ Route::prefix('v1')->group(function () {
     Route::post('/optvalidation', [AuthController::class,'optValidation']);
     Route::post('/resetpassword', [AuthController::class, 'resetPassword']);
 
+
+
+
     // Protected Routes
     Route::middleware('jwt.auth')->group(function () {
 
@@ -28,14 +36,25 @@ Route::prefix('v1')->group(function () {
         Route::get('/profile', [AuthController::class, 'profile']);
         Route::post('/logout', [AuthController::class, 'logout']);
 
+        //Admin Summery data
+        Route::apiResource('admin/dashboard', DashboardController::class);
+
         // Department CRUD
         Route::apiResource('departments', DepartmentController::class);
 
+        //Shift CRUD
+        Route::apiResource('shifts', ShiftController::class);
+
         // Employee CRUD
         Route::apiResource('employees', EmployeeController::class);
+        Route::get('employee/attributes',[EmployeeController::class,'employeeAttributes']);
 
         // Attendance
         Route::apiResource('attendances', AttendanceController::class);
+        Route::post('/attendance/filter', [AttendanceController::class, 'attendanceFilter']);
+        Route::post('/attendance/filter/{id}', [AttendanceController::class, 'attendanceFilterPersonal']);
+        Route::get('/employee/attendance/{id}', [AttendanceController::class, 'employeeAttendance']);
+        Route::post('attendance/sync',[AttendanceController::class,'attendanceSyncer']);
 
         // Leave
         Route::apiResource('leaves', LeaveController::class);
@@ -45,5 +64,37 @@ Route::prefix('v1')->group(function () {
 
         // Documents
         Route::apiResource('documents', DocumentController::class);
+
+        //Holiday
+        Route::apiResource('holiday', HolidayController::class);
+
+        //Notice
+        Route::apiResource('notice', NoticeController::class);
     });
+
+
+
+    //Automation routes (Dont Touch)
+    // Attendance taking from machiene
+    Route::post('/local/attendance', [AttendanceController::class, 'bulkStore']);
+
+    //Setting Users to machien
+    Route::get('/local/set/users', function () {
+        $employees = Employee::all(); // fetch all employees
+
+        $result = $employees->map(function($emp) {
+            return [
+                "uid" => (int) preg_replace('/\D/', '', $emp->eid),
+                "userId" => (int) $emp->id,
+                "name" => $emp->fname . " " . $emp->lname,
+                "role" => 0,
+                "password" => "",
+                "cardno" => "000000000"
+            ];
+        });
+
+        return response()->json($result);
+    });
+
+
 });
