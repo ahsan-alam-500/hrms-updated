0e0a68f2f9b8b98607f9b20d722a1b3774995144
Ahsan Alam <ahsansfiverr@gmail.com>
2025-08-28 10:54:59 +0600

    bug fixing

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,3 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-
-Schedule::command(TestCommand::class)->everySecond();
